9b99fea305850dd7f7e24c29072ea383c1f79b58
tedrfg2541 <44422479+tedrfg2541@users.noreply.github.com>
2019-04-16 18:53:03 +0800

    Add files via upload

--- MATLAB/test.m
+++ MATLAB/test.m
@@ -15,48 +15,79 @@ for i=1:data_length
     data(i,2) = str2double(token(3));
     data(i,3) = str2double(token(4));
 end
+objs = instrfind;
+fclose(objs);
 
+
+x = 0:0.01:9.99;
+y = data(:,3);
+plot(x,y)
 %所需範圍取值
 dt = 0.01;
-data_h = ;
-data_t = ;
-data_length = data_t-data_h;
+data_h = 460;
+data_t = 700;
+data_length = data_t-data_h+1;
+x = x(data_h:data_t);
+%平滑公式
+for i=data_h+1:data_t
+    data(i,1) = data(i,1)*0.3 + data(i-1,1)*0.7;
+    data(i,2) = data(i,2)*0.3 + data(i-1,2)*0.7;
+    data(i,3) = data(i,3)*0.3 + data(i-1,3)*0.7;
+end
 %輸入電壓
-v = ;
+v = 0;
 
 U = zeros(data_length,1) + v;
 thetaA = data(data_h:data_t,1);
 thetaB = data(data_h:data_t,2);
-sigma = data(data_h:data_t,3);
+phi = data(data_h:data_t,3);
 
-dthetaA = zeros(data_length);
-dthetaB = zeros(data_length);
-dsigma = zeros(data_length);
-for i=1 : (size(sigma)-1)
+dthetaA = zeros(data_length,1);
+dthetaB = zeros(data_length,1);
+dphi = zeros(data_length,1);
+for i=1 : (size(phi)-1)
     dthetaA(i) = (thetaA(i+1)-thetaA(i))/dt;
     dthetaB(i) = (thetaB(i+1)-thetaB(i))/dt;
-    dsigma(i) = (sigma(i+1)-sigma(i))/dt;
+    dphi(i) = (phi(i+1)-phi(i))/dt;
 end
 dthetaA(data_length) = (thetaA(data_length)-thetaA(data_length-1))/dt;
 dthetaB(data_length) = (thetaB(data_length)-thetaB(data_length-1))/dt;
-dsigma(data_length) = (sigma(data_length)-sigma(data_length-1))/dt;
+dphi(data_length) = (phi(data_length)-phi(data_length-1))/dt;
 
 
-ddthetaA = zeros(data_length);
-ddthetaB = zeros(data_length);
-ddsigma = zeros(data_length);
-for i=1 : (size(sigma)-1)
+ddthetaA = zeros(data_length,1);
+ddthetaB = zeros(data_length,1);
+ddphi = zeros(data_length,1);
+for i=1 : (size(phi)-1)
     ddthetaA(i) = (dthetaA(i+1)-dthetaA(i))/dt;
     ddthetaB(i) = (dthetaB(i+1)-dthetaB(i))/dt;
-    ddsigma(i) = (dsigma(i+1)-dsigma(i))/dt;
+    ddphi(i) = (dphi(i+1)-dphi(i))/dt;
 end
 ddthetaA(data_length) = (dthetaA(data_length)-dthetaA(data_length-1))/dt;
 ddthetaB(data_length) = (dthetaB(data_length)-dthetaB(data_length-1))/dt;
-ddsigma(data_length) = (dsigma(data_length)-dsigma(data_length-1))/dt;
+ddphi(data_length) = (dphi(data_length)-dphi(data_length-1))/dt;
+
+E = ddphi;
+G1 = zeros(data_length,5);
+for i=1 : (data_length)
+    G1(i,1) = -ddthetaA(i);
+    G1(i,2) = -2*cos(phi(i))*ddphi(i)+sin(2*phi(i))*sec(phi(i))*(dphi(i)^2);
+    G1(i,3) = -dthetaA(i);
+    G1(i,4) = dphi(i);
+    G1(i,5) = U(i);
+end
+A = pinv(transpose(G1)*G1)*transpose(G1)*E;
+
+G2 = zeros(data_length,5);
+for i=1 : (data_length)
+    G2(i,1) = -ddthetaA(i);
+    G2(i,2) = -cos(phi(i))*ddphi(i);
+    G2(i,3) = dthetaA(i)-dphi(i);
+    G2(i,4) = sin(phi(i));
+    G2(i,5) = U(i);
+end
+B = pinv(transpose(G2)*G2)*transpose(G2)*E;
 
 
-E = ddsigma;
-G = [-ddthetaA, -2*cos(sigma)*ddsigma+sin(2*sigma)*sec(sigma)*(dsigma^2), -dthetaA, dsigma, U];
-    
 objs = instrfind;
 fclose(objs);
