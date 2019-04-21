aed3722ad2a65dbc69d626dc287575a9410b782b
lovemachune <38471026+lovemachune@users.noreply.github.com>
2019-04-21 23:11:36 +0800

    Add files via upload

--- MATLAB/animation.m
+++ MATLAB/animation.m
@@ -4,13 +4,16 @@ axis(gca,'equal');
 axis([-5 5 -1 5]);
 x = 0;
 y = 4;
+simulink_pi = [1/3,1/3,1/3,1/3,1/3,1/3,1/3,1/3,1/3,1/3];
 center = [0,0];
-for i=1:500
+for i=1:10
     cir = viscircles(center,1);
-	lin = line([0, 0],[x, y]);
-    x = x*cos(simulink_pi(i))-y*sin(simulink_pi(i));
-	y = x*sin(simulink_pi(i))-y*cos(simulink_pi(i));
-    pause(0.01);
+	lin = line([0, x],[0, y]);
+    t1 = x*cos(pi*simulink_pi(i))-y*sin(pi*simulink_pi(i));
+	t2 = x*sin(pi*simulink_pi(i))+y*cos(pi*simulink_pi(i));
+    x =t1;
+    y =t2;
+    pause(1);
     delete(cir);
     delete(lin);
 end
