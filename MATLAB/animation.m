2c0c3b50382bee14536e4161840bff59410baf40
lovemachune <38471026+lovemachune@users.noreply.github.com>
2019-04-24 23:39:04 +0800

    Add files via upload

--- MATLAB/animation.m
+++ MATLAB/animation.m
@@ -7,13 +7,13 @@ y = 4;
 center_x = 0;
 simulink_pi = [90*pi/180,80*pi/180,70*pi/180,60*pi/180,50*pi/180,40*pi/180,30*pi/180,20*pi/180,10*pi/180,0];
 simulink_theta = [60*pi/180,60*pi/180,60*pi/180,60*pi/180,60*pi/180,60*pi/180,60*pi/180,60*pi/180,60*pi/180,60*pi/180];
-for i=1:10
+for i=1:10000
     cir = viscircles([center_x,0],1);
 	lin = line([center_x, x],[0, y]);
-    x = 4*cos(simulink_pi(i));
-	y = 4*sin(simulink_pi(i));
-	center_x = center_x+sin(simulink_theta(i));
-    pause(1);
+    x = 4*sin(out.simulink_phi(i));
+	y = 4*cos(out.simulink_phi(i));
+	center_x = center_x+sin(out.simulink_theta(i));
+    pause(0.00001);
     delete(cir);
     delete(lin);
 end
